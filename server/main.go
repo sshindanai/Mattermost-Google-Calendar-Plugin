7c50c6735cb125c1f8263dc4afbf0e3121fdacfb
sshindanai <sshindanai.m@gmail.com>
2023-02-23 12:59:51 +0700

    update: main.go

--- server/main.go
+++ server/main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"github.com/sshindanai/Mattermost-Google-Calendar-Plugin/server/plugin"
+
+	pluginapi "github.com/mattermost/mattermost-server/v5/plugin"
+)
+
 func main() {
+	pluginapi.ClientMain(&plugin.Plugin{})
 }
